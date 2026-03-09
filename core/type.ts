655781a3ba0f28c5898069e06302a1b2edb40f8c
qikang.yuan <me@qkang.top>
2026-03-09 17:05:59 +0800

    1

--- core/type.ts
+++ core/type.ts
@@ -14,5 +14,32 @@ export type BlockNode = {
 export type ComponentNode = {
     type: 'component'
     props: Record<string, any>
+
+}
+
+
+export type ElementNodeBase = {
+    type: 'element'
+    style?: Record<string, string | number>
+
+
+}
+
+export type ViewElementNode = {
+    element: 'view'
+} & ElementNodeBase
+
+export type TextElementNode = {
+    element: 'text'
+} & ElementNodeBase
+
+export type ImgElementNode = {
+    element: 'img'
+} & ElementNodeBase
+
+export type ElementNode = ViewElementNode | TextElementNode | ImgElementNode
+
+export type SlotNode = {
+
 }
 
