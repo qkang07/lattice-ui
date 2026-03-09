254eb06a506b8efd1db7efae527fb9dcafc54d39
qikang.yuan <me@qkang.top>
2026-03-09 16:27:10 +0800

    s

--- core/type.ts
+++ core/type.ts
@@ -1,1 +1,18 @@
-export type NodeType = 'block' | 'component'
+export type NodeType = 'block' | 'component' | 'element' | 'array' | 'slot';
+
+export type NodeBase = {
+    type: NodeType
+
+}
+
+
+export type BlockNode = {
+    type: 'block'
+    props: Record<string, any>
+} 
+
+export type ComponentNode = {
+    type: 'component'
+    props: Record<string, any>
+}
+
